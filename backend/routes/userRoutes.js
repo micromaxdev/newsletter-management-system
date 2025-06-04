0c0ee3e6b043a180a2cdaeee9383073485ee8a99
MichelleUma <128358394+MichelleUma@users.noreply.github.com>
2025-06-05 09:02:02 +1000

    Add files via upload

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,3 +1,4 @@
+// working code
 const express = require("express");
 const router = express.Router();
 const { registerUser } = require("../controllers/userController");
@@ -5,3 +6,24 @@ const { registerUser } = require("../controllers/userController");
 router.post("/", registerUser);
 
 module.exports = router;
+
+ 
+// routes/emailRoutes.js
+// const express = require("express");
+// const router = express.Router();
+// const Email = require("../models/Email");
+
+// // @desc    Get all saved emails from MongoDB
+// // @route   GET /api/emails/saved
+// // @access  Public
+// router.get("/saved", async (req, res) => {
+//   try {
+//     const emails = await Email.find().sort({ date: -1 });
+//     res.json(emails);
+//   } catch (err) {
+//     res.status(500).json({ error: "Failed to fetch saved emails" });
+//   }
+// });
+
+// module.exports = router;
+
