565f5a8e26d1cf752e157932034a2e1904dc42f7
MichelleUma <128358394+MichelleUma@users.noreply.github.com>
2025-07-02 12:09:07 +1000

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,40 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
-//Working
-// const mongoose = require("mongoose");
-
-// // Add this line to suppress the warning:
-// mongoose.set('strictQuery', true);
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
@@ -54,44 +18,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-// config/db.js
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     process.exit(1); // Exit process with failure
-//   }
-// };
-
-// module.exports = connectDB;
-
-
-
-// const mongoose = require('mongoose');
-
-// // Add this line to suppress the warning:
-// mongoose.set('strictQuery', true);
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('MongoDB Connected: ' + mongoose.connection.host);
-//   } catch (error) {
-//     console.error(error);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
